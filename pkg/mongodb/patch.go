d8aa380d1ba0efc5a77fe1833e9c4f9034e894dc
Nam Truong Le <nam.truong.le@sap.com>
2022-12-19 11:01:53 +0100

    handle object id when convert patch to mongodb update query

--- pkg/mongodb/patch.go
+++ pkg/mongodb/patch.go
@@ -4,21 +4,55 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/pkg/errors"
+	"github.com/samber/lo"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/nam-truong-le/lambda-utils-go/pkg/rest"
 )
 
 // UpdateFromPatch convert JSON Patch to mongodb update object
-func UpdateFromPatch(req rest.PatchRequest) (interface{}, error) {
+func UpdateFromPatch(req rest.PatchRequest, objIDPaths []string) (interface{}, error) {
 	update := bson.A{}
 	for _, patch := range req {
 		if patch.OP != "replace" {
 			return nil, fmt.Errorf("JSON patch operation [%s] not supported", patch.OP)
 		}
+
+		key := jsonPathToMongoDBKey(patch.Path)
+		val := patch.Value
+		if lo.Contains(objIDPaths, key) {
+			var err error
+			switch valTyped := patch.Value.(type) {
+			case string:
+				val, err = primitive.ObjectIDFromHex(valTyped)
+				if err != nil {
+					return nil, fmt.Errorf("failed to parse mongodb object id [%s]: %s", valTyped, err)
+				}
+			case []interface{}:
+				uuids := make([]primitive.ObjectID, 0)
+				for _, id := range valTyped {
+					switch vid := id.(type) {
+					case string:
+						uuid, err := primitive.ObjectIDFromHex(vid)
+						if err != nil {
+							return nil, errors.Wrap(err, fmt.Sprintf("[%s] is not an uuid", id))
+						}
+						uuids = append(uuids, uuid)
+					default:
+						return nil, fmt.Errorf("path [%s] is marked as uuid but got non string element: %+v", key, vid)
+					}
+				}
+				val = uuids
+			default:
+				return nil, fmt.Errorf("path [%s] is marked as uuid but got: %+v", key, valTyped)
+			}
+		}
+
 		update = append(update, bson.M{
 			"$set": bson.M{
-				jsonPathToMongoDBKey(patch.Path): patch.Value,
+				key: val,
 			},
 		})
 	}
--- pkg/mongodb/path_test.go
+++ pkg/mongodb/path_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/nam-truong-le/lambda-utils-go/pkg/mongodb"
 	"github.com/nam-truong-le/lambda-utils-go/pkg/rest"
@@ -27,7 +28,7 @@ func TestUpdateFromPatch(t *testing.T) {
 			Path:  "/c",
 			Value: 1234,
 		},
-	})
+	}, nil)
 	assert.NoError(t, err)
 	assert.Equal(t, bson.A{
 		bson.M{
@@ -43,6 +44,73 @@ func TestUpdateFromPatch(t *testing.T) {
 		bson.M{
 			"$set": bson.M{
 				"c": 1234,
+			},
+		},
+	}, update)
+}
+
+// tests from old asia loop implementation
+func TestMongoUpdateFromJSONPatch(t *testing.T) {
+	patch1 := rest.Patch{
+		OP:    "replace",
+		Path:  "/a/b/c",
+		Value: "string",
+	}
+	patch2 := rest.Patch{
+		OP:    "replace",
+		Path:  "/a/b/d",
+		Value: 1234,
+	}
+	patch3 := rest.Patch{
+		OP:    "replace",
+		Path:  "/a/c/e",
+		Value: true,
+	}
+	objectID1 := primitive.NewObjectID()
+	objectID1String := objectID1.Hex()
+	patchObjectID := rest.Patch{
+		OP:    "replace",
+		Path:  "/objectId1/a",
+		Value: objectID1String,
+	}
+	objectID2 := primitive.NewObjectID()
+	objectID2String := objectID2.Hex()
+	objectID3 := primitive.NewObjectID()
+	objectID3String := objectID3.Hex()
+	patchObjectIDArray := rest.Patch{
+		OP:    "replace",
+		Path:  "/objectId1/array",
+		Value: []interface{}{objectID2String, objectID3String},
+	}
+	patches := rest.PatchRequest{patch1, patch2, patch3, patchObjectID, patchObjectIDArray}
+	objectIDPaths := []string{"objectId1.a", "objectId1.array"}
+	update, err := mongodb.UpdateFromPatch(patches, objectIDPaths)
+
+	assert.NoError(t, err)
+	assert.Equal(t, bson.A{
+		bson.M{
+			"$set": bson.M{
+				"a.b.c": "string",
+			},
+		},
+		bson.M{
+			"$set": bson.M{
+				"a.b.d": 1234,
+			},
+		},
+		bson.M{
+			"$set": bson.M{
+				"a.c.e": true,
+			},
+		},
+		bson.M{
+			"$set": bson.M{
+				"objectId1.a": objectID1,
+			},
+		},
+		bson.M{
+			"$set": bson.M{
+				"objectId1.array": []primitive.ObjectID{objectID2, objectID3},
 			},
 		},
 	}, update)
