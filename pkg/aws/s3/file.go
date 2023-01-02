aaa5a9f1c4e76bcf5fdef04ffea4117d873507e6
Nam Truong Le <lenamtruong@gmail.com>
2023-01-02 05:22:19 +0100

    feature: write file to s3 (#35)
    Co-authored-by: Nam Truong Le <nam.truong.le@sap.com>

--- pkg/aws/s3/file.go
+++ pkg/aws/s3/file.go
@@ -1,6 +1,7 @@
 package s3
 
 import (
+	"bytes"
 	"context"
 	"io"
 
@@ -36,19 +37,51 @@ func ReadFile(ctx context.Context, bucket, key string) ([]byte, error) {
 		Key:    aws.String(key),
 	})
 	if err != nil {
-		log.Errorf("Failed to read s3 file [%s] [%s]", bucket, key)
+		log.Errorf("Failed to read s3 file [%s] [%s]: %s", bucket, key, err)
 		return nil, err
 	}
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-			log.Errorf("Failed to close s3 file [%s] [%s]", bucket, key)
+			log.Errorf("Failed to close s3 file [%s] [%s]: %s", bucket, key, err)
 		}
 	}(res.Body)
 	data, err := io.ReadAll(res.Body)
 	if err != nil {
-		log.Errorf("Failed to read s3 file content [%s] [%s]", bucket, key)
+		log.Errorf("Failed to read s3 file content [%s] [%s]: %s", bucket, key, err)
 		return nil, err
 	}
 	return data, nil
+}
+
+func WriteFileBucketSSM(ctx context.Context, bucketSSM, key string, file []byte) error {
+	log := logger.FromContext(ctx)
+	log.Infof("Write s3 file [ssm:%s] [%s]", bucketSSM, key)
+
+	bucket, err := ssm.GetParameter(ctx, bucketSSM, false)
+	if err != nil {
+		log.Errorf("Failed to get bucket name from SSM [%s]: %s", bucketSSM, err)
+		return err
+	}
+	return WriteFile(ctx, bucket, key, file)
+}
+
+func WriteFile(ctx context.Context, bucket, key string, file []byte) error {
+	log := logger.FromContext(ctx)
+	log.Infof("Write s3 file [%s] [%s]", bucket, key)
+
+	c, err := NewClient(ctx)
+	if err != nil {
+		return err
+	}
+	_, err = c.PutObject(ctx, &s3.PutObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+		Body:   bytes.NewReader(file),
+	})
+	if err != nil {
+		log.Errorf("Faile to write s3 file [%s] [%s]: %s", bucket, key, err)
+		return err
+	}
+	return nil
 }
