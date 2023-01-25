9905d0ca4c20cd18345781a41a514e3c7ebe92ae
Nam Truong Le <nam.truong.le@sap.com>
2023-01-25 16:22:30 +0100

    fix: add cleanup hook for SQS processing

--- pkg/aws/sqs/process.go
+++ pkg/aws/sqs/process.go
@@ -12,20 +12,34 @@ import (
 )
 
 type ProcessFunction func(ctx context.Context, rec *events.SQSMessage) error
+type CleanUpFunction func(ctx context.Context) error
 
-func Process(ctx context.Context, e *events.SQSEvent, name string, processFn ProcessFunction) (*events.SQSEventResponse, error) {
+type ProcessOptions struct {
+	Name    string
+	Process ProcessFunction
+	CleanUp CleanUpFunction
+}
+
+func Process(ctx context.Context, e *events.SQSEvent, opts ProcessOptions) (*events.SQSEventResponse, error) {
 	stage, ok := os.LookupEnv("ENV")
 	if !ok {
 		return nil, fmt.Errorf("environment variable ENV doesn't exist")
 	}
 	stageCtx := context.WithValue(ctx, mycontext.FieldStage, stage)
-	stageCtx = context.WithValue(stageCtx, mycontext.FieldFunction, name)
-	return process(stageCtx, e, processFn)
+	stageCtx = context.WithValue(stageCtx, mycontext.FieldFunction, opts.Name)
+	return process(stageCtx, e, opts.Process, opts.CleanUp)
 }
 
-func process(ctx context.Context, e *events.SQSEvent, processFn ProcessFunction) (*events.SQSEventResponse, error) {
+func process(ctx context.Context, e *events.SQSEvent, processFn ProcessFunction, cleanFn CleanUpFunction) (*events.SQSEventResponse, error) {
 	log := logger.FromContext(ctx)
 	log.Infof("Get %d sns events from SQS", len(e.Records))
+
+	defer func() {
+		err := cleanFn(ctx)
+		if err != nil {
+			log.Errorf("Failed to clean up SQS processing: %s", err)
+		}
+	}()
 
 	failures := make([]events.SQSBatchItemFailure, 0)
 	for i, record := range e.Records {
